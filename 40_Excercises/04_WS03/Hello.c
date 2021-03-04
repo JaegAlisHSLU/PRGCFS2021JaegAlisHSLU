1725afc47b3ffcd6f3044b7a3d36844a69341121
JaegAlis <blabla@example.com>
2021-03-04 20:08:50 +0100

    2

--- 40_Excercises/04_WS03/Hello.c
+++ 40_Excercises/04_WS03/Hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>		//ruft Library stdio.h auf
 
-int main()
-{  
-    printf("Hello World\n");
-    return 0;
-}
+int main()			//integer Funktion mit Namen main
+{  				//eröffnet Funktion
+    printf("Hello World\n");	//Schreibt "Hello World" und \n generiert eine neue Zeile
+    
+    return 0;			//gibt Wert 0 zurueck
+}				//schliesst Funktion
